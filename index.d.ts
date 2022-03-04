1f261755abad3c9501278a003b38269b0c2d9921
Erick Haendel <erickhaendel@gmail.com>
2022-03-03 23:15:49 -0300

    Ajuste de bug setURL

--- index.d.ts
+++ index.d.ts
@@ -76,11 +76,12 @@ export class MessageBuilder {
   setText(text: string): this;
   setAuthor(author?: string, authorImage?: string, authorUrl?: string): this;
   setTitle(title: string): this;
-  setUrl(url: string): this;
+setURL(url: string): this;
+  setColor(color: string | number): this;
   setThumbnail(thumbnailUrl: string): this;
   setImage(image: string): this;
   setTimestamp(): this;
-  setColor(color: number): this;
+
   setDescription(description: string): this;
   addField(fieldName: string, fieldValue: string, inline?: boolean): this;
   setFooter(footer: string, footerImage?: string): this;
